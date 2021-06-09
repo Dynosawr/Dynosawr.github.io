d86a6dd95386fa7c3fc65c9158092611ef0d25cb
blazter1324 <82094548+Dynosawr@users.noreply.github.com>
2021-06-09 09:23:39 +0700

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,13 +1,13 @@
 const options = {
-  bottom: '64px', // default: '32px'
+  bottom: '32px', // default: '32px'
   right: '32px', // default: '32px'
   left: 'unset', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
+  time: '0.3s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
+  label: 'ㅤ', // default: ''
   autoMatchOsTheme: true // default: true
 }
 
