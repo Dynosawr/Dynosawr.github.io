06fd2d0cf5c8ee0d1e2b9d1cdb5283467733c98b
blazter1324 <82094548+Dynosawr@users.noreply.github.com>
2021-06-21 20:50:08 +0700

    Create index.php

---
+++ chat/index.php
@@ -0,0 +1,15 @@
+---
+layout: default
+title: ???
+---
+<html lang="{{ site.lang | default: "en-US" }}">
+  <head>
+	  <title>Chit Chat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+  </head>
+  <body>
+ <?php
+    echo "ping pong";
+    ?>
+    </body>
